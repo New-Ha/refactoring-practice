787b9a1add3a9bc4ef1a0cbea59574d20ce4775b
UHa <https00200@gmail.com>
2023-08-13 22:19:47 +0900

    💩 Add Bad Code Reason Comments

--- ch6/6-1.js
+++ ch6/6-1.js
@@ -1,3 +1,7 @@
+ /**
+  * 함수 추출하기 
+  */
+
 export function printOwing(invoice) {
   let outstanding = 0;
 
@@ -6,12 +10,15 @@ export function printOwing(invoice) {
   console.log('***********************');
 
   // calculate outstanding
+  // outstanding(채무액)에 orders의 각 요소의 amount 값을 더한다.
   for (const o of invoice.orders) {
     outstanding += o.amount;
   }
 
   // record due date
+  // date data를 불러온다.
   const today = new Date();
+  // date를 변형해서 dueDate 로 저장한다.
   invoice.dueDate = new Date(
     today.getFullYear(),
     today.getMonth(),
@@ -19,6 +26,7 @@ export function printOwing(invoice) {
   );
 
   //print details
+  // 고객명과, 주문량, 주문일을 프린트
   console.log(`name: ${invoice.customer}`);
   console.log(`amount: ${outstanding}`);
   console.log(`due: ${invoice.dueDate.toLocaleDateString()}`);
