0d52aff6696e0de9b302da7c8354a41328f4b8fa
UHa <https00200@gmail.com>
2023-08-16 17:34:40 +0900

    💩 Bad code reason comments

--- ch_06/6-09.js
+++ ch_06/6-09.js
@@ -1,3 +1,7 @@
+// data와 사용처(6-09-client1, 2, 3)가 흩어져 있는 나쁜 코드
+// 코드 중복 + 재사용성 떨어지고 + 유지보수 안됨
+// => 응집시켜 클래스화 + 캡슐화
+
 const reading = { customer: 'ivan', quantity: 10, month: 5, year: 2017 };
 
 export function acquireReading() {
