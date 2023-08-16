2d76960686dd8ea81d46d217ed86333280b0ce3b
UHa <https00200@gmail.com>
2023-08-16 17:29:20 +0900

    ♻️ Refactoring code 2

--- ch_06/6-06-use.js
+++ ch_06/6-06-use.js
@@ -1,9 +1,8 @@
 //6-06 함수 사용처
 import { getDefaultOwner } from './6-6.js';
 
-// 함수를 호출해 owner에 할당하고
 const owner = getDefaultOwner();
-// owner의 내용을 바꾸면 함수의 객체도 바뀐다. -> 객체가 참조값이어서
-owner.firstName = '엘리';
+// class로 만들어 전달된 경우, 에러가 난다. getter만 있고 setter는 없기 때문
+// owner.firstName = '엘리';
 console.log(owner);
 console.log(getDefaultOwner());
--- ch_06/6-06.js
+++ ch_06/6-06.js
@@ -1,8 +1,25 @@
-// ver 1
-// 객체의 주소를 바로 반환하는 대신, 복사해 새로운 객체를 반환!
-// -> 1) Object.assign;
-//    2) {...defaultOwner}
-let defaultOwner = { firstName: '마틴', lastName: '파울러' };
+// ver 2
+//{...defaultOwner}은 얕은 복사가 일어나기 때문에, 중첩객체라면 내부 객체는 그대로 참조값을 써서 위험할 수 있다.
+// class를 만들면 get만 제공하므로 수정할 수 없는 클래스가 된다.
+
+class Person {
+  #lastName;
+  #firstName;
+  constructor(data) {
+    this.#lastName = data.lastName;
+    this.#firstName = data.firstName;
+  }
+
+  get lastName() {
+    return this.#lastName;
+  }
+
+  get firstName() {
+    return this.#firstName;
+  }
+}
+
+let defaultOwner = new Person({ firstName: '마틴', lastName: '파울러' });
 
 export function getDefaultOwner() {
   return {...defaultOwner};
