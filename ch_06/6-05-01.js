919ff1c5659f742802fbfee8982a86f5554b6fa7
UHa <https00200@gmail.com>
2023-08-16 10:13:01 +0900

    ♻️ Refactoring code with comments

--- ch_06/6-05-01.js
+++ ch_06/6-05-01.js
@@ -1,3 +1,8 @@
-export function circum(radius) {
+/** 함수선언 바꾸기 */
+
+//너무 축약해서 사용하지 말자
+// F2 : rename symbol -> 함수선언, 사용하는 소스코드 모두 이름을 바꿔준다.
+// 원 -> 원주율 로 고쳐준다.
+export function circumference(radius) {
   return 2 * Math.PI * radius;
 }
