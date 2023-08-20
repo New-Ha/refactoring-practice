8bf01523b191e903d4d6bc18fa22479a5dc375d8
UHa <https00200@gmail.com>
2023-08-20 23:32:12 +0900

    ♻️ refactoring step2

--- ch_06/6-11-2.js
+++ ch_06/6-11-2.js
@@ -1,11 +1,22 @@
 import fs from 'fs';
 
-// 스크립트를 사용하는 경우 보통 크게 두 단계로 나뉜다.
-// 사용자에게 입력을 받아옴 -> 유효성 검사
-// 필요한 로직을 처리
+//1. run 함수를 만들어 노드의 process dependency를 제거.
+run(process.argv);
 
-// node에서 제공하는 process 자체에 접근하면 test하기가 어려워 함수로 따로 빼준다.
-function fun(args) {
+function run(args) {
+    // 2. 사용자에게 입력을 받아 유효성을 검사하는 부분을 쪼갠다.
+    const command = parseCommand(args);
+
+    const rawData = fs.readFileSync(fileName);
+    const orders = JSON.parse(rawData);
+    if (command.countReadyOnly) {
+        console.log(orders.filter(order => order.status === 'ready').length);
+    } else {
+        console.log(orders.length);
+    }
+}
+
+function parseCommand(args) {
     if (!args.argv[2]) {
         throw new Error('파일 이름을 입력하세요');
     }
@@ -15,12 +26,7 @@ function fun(args) {
         throw new Error('파일이 존재하지 않습니다');
     }
 
-    //--------------------------------------
-    const rawData = fs.readFileSync(fileName);
-    const orders = JSON.parse(rawData);
-    if (args.argv.includes('-r')) {
-        console.log(orders.filter(order => order.status === 'ready').length);
-    } else {
-        console.log(orders.length);
-    }
+    const countReadyOnly = args.includes('-r');
+
+    return { fileName, countReadyOnly };
 }
