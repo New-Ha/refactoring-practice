09954a1480a1c2e8e3069d0864b5a16aeab442ff
UHa <https00200@gmail.com>
2023-08-20 23:27:50 +0900

    ♻️ refactoring step1

--- ch_06/6-11-2.js
+++ ch_06/6-11-2.js
@@ -4,20 +4,23 @@ import fs from 'fs';
 // 사용자에게 입력을 받아옴 -> 유효성 검사
 // 필요한 로직을 처리
 
-if (!process.argv[2]) {
-    throw new Error('파일 이름을 입력하세요');
-}
+// node에서 제공하는 process 자체에 접근하면 test하기가 어려워 함수로 따로 빼준다.
+function fun(args) {
+    if (!args.argv[2]) {
+        throw new Error('파일 이름을 입력하세요');
+    }
 
-const fileName = `./${process.argv[2]}.json`;
-if (!fs.existsSync(fileName)) {
-    throw new Error('파일이 존재하지 않습니다');
-}
+    const fileName = `./${args.argv[2]}.json`;
+    if (!fs.existsSync(fileName)) {
+        throw new Error('파일이 존재하지 않습니다');
+    }
 
-//--------------------------------------
-const rawData = fs.readFileSync(fileName);
-const orders = JSON.parse(rawData);
-if (process.argv.includes('-r')) {
-    console.log(orders.filter(order => order.status === 'ready').length);
-} else {
-    console.log(orders.length);
+    //--------------------------------------
+    const rawData = fs.readFileSync(fileName);
+    const orders = JSON.parse(rawData);
+    if (args.argv.includes('-r')) {
+        console.log(orders.filter(order => order.status === 'ready').length);
+    } else {
+        console.log(orders.length);
+    }
 }
