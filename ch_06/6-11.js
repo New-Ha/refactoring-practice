c8e8c2a88111a41387651c27e3233676a19cb432
UHa <https00200@gmail.com>
2023-08-20 23:02:26 +0900

    ♻️ refactoring challenge code

--- ch_06/6-11.js
+++ ch_06/6-11.js
@@ -1,29 +1,35 @@
 export function priceOrder(product, quantity, shippingMethod) {
-  const basePrice = product.basePrice * quantity;
-  const discount =
-    Math.max(quantity - product.discountThreshold, 0) *
-    product.basePrice *
-    product.discountRate;
-  const shippingPerCase =
-    basePrice > shippingMethod.discountThreshold
-      ? shippingMethod.discountedFee
-      : shippingMethod.feePerCase;
-  const shippingCost = quantity * shippingPerCase;
-  const price = basePrice - discount + shippingCost;
-  return price;
+    const baseCalPrice = getDiscountedPrice(product, quantity);
+    const shippingCost = calShippingCost(baseCalPrice, quantity, shippingMethod);
+    const price = baseCalPrice.basePrice - baseCalPrice.discount + shippingCost;
+    return price;
+}
+
+function getDiscountedPrice(product, quantity) {
+    const basePrice = product.basePrice * quantity;
+    const discount = Math.max(quantity - product.discountThreshold, 0) * product.basePrice * product.discountRate;
+    return { basePrice: basePrice, discount: discount };
+}
+
+function calShippingCost(baseCalPrice, quantity, shippingMethod) {
+    const shippingPerCase =
+        baseCalPrice.getDiscountedPrice > shippingMethod.discountThreshold
+            ? shippingMethod.discountedFee
+            : shippingMethod.feePerCase;
+    return quantity * shippingPerCase;
 }
 
 // 사용 예:
 const product = {
-  basePrice: 10,
-  discountRate: 0.1,
-  discountThreshold: 10,
+    basePrice: 10,
+    discountRate: 0.1,
+    discountThreshold: 10,
 };
 
 const shippingMethod = {
-  discountThreshold: 20,
-  feePerCase: 5,
-  discountedFee: 3,
+    discountThreshold: 20,
+    feePerCase: 5,
+    discountedFee: 3,
 };
 
 const price = priceOrder(product, 5, shippingMethod);
