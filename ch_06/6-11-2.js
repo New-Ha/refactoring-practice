899693160ba809fd94be616dcba71772634efbcf
UHa <https00200@gmail.com>
2023-08-20 23:40:26 +0900

    ♻️ final code arrangement

--- ch_06/6-11-2.js
+++ ch_06/6-11-2.js
@@ -3,36 +3,23 @@ import fs from 'fs';
 run(process.argv);
 
 function run(args) {
-    const command = parseCommand(args);
-
-    //3. 필요한 로직을 따로 빼서 처리
-    countOrders(command);
-
-    //한 줄로 인라인 할수도 있다.
-    //countOrders(parseCommand(args))
+    countOrders(parseCommand(args));
 }
 
 function parseCommand(args) {
     if (!args.argv[2]) {
         throw new Error('파일 이름을 입력하세요');
     }
-
     const fileName = `./${args.argv[2]}.json`;
     if (!fs.existsSync(fileName)) {
         throw new Error('파일이 존재하지 않습니다');
     }
-
-    // const countReadyOnly = args.includes('-r'); -> return문에 인라인
-
     return { fileName, countReadyOnly: args.includes('-r') };
 }
 
-function countOrders(command) {
-    const rawData = fs.readFileSync(command.fileName);
+function countOrders({ fileName, countReadyOnly }) {
+    const rawData = fs.readFileSync(fileName);
     const orders = JSON.parse(rawData);
-    if (command.countReadyOnly) {
-        console.log(orders.filter(order => order.status === 'ready').length);
-    } else {
-        console.log(orders.length);
-    }
+    const filtered = countReadyOnly ? orders.filter(order => order.status === 'ready') : orders;
+    console.log(filtered.length);
 }
